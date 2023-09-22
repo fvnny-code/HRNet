7f434acea9900e9bf3ddd2e3937457c66247c600
fvnny-code <f.codecco.grando@gmail.com>
2023-09-22 21:32:09 +0200

    uninstall of storybook dependencies

--- src/components/modal/Modal.stories.tsx
+++ src/components/modal/Modal.stories.tsx
@@ -1,38 +1,27 @@
-import {Modal} from "./Modal"
+import type { Meta, StoryObj } from "@storybook/react"
 
-export default {
-title: "UI/Modal",
-component: Modal,
+import { Modal } from "./Modal"
 
-}
 
-export const myModal  =() => {
-    return <div> Pouet pouet</div>
-}
 
-const Template =()=> (
-    <Modal>
-        <div className="backdrop">
-      <div
-        className="wrapper"
-        aria-modal
-        aria-labelledby="Confirmation"
-        tabIndex={-1}
-        role="doalog"
-      >
-        <div className="styledModal">
-          <div className="header-modal">
-            <button className="closeButton">
-              X
-            </button>
-          </div>
-          <div className="content"></div>
-        </div>
-      </div>
-    </div>
-    </Modal>
-)
-export const Default = Template.bind({})
 
+
+const meta: Meta<typeof Modal> = {
+    component: Modal,
+    argTypes: {
+        
+    }
+  };
+  
+  export default meta;
+  type Story = StoryObj<typeof Modal>;
+
+  export const ModalStory : Story = {
+    args: {
+        isShown : true,
+        onHide : ()=> {},
+        message : "string"
+    }
+  }
 
 
